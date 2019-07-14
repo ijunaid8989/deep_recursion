4b8cd4150208000c6361db4a70794f0f5968ddcd
Junaid Farooq <junaid@evercam.io>
2019-07-14 15:20:25 +0500

    First level deps.

--- lib/deep.ex
+++ lib/deep.ex
@@ -1,3 +1,18 @@
 defmodule Deep do
-  @registry "http://registry.npmjs.org/#{}/latest"
+  @registry "http://registry.npmjs.org/"
+
+  alias HTTPoison.Response, as: Resp
+
+  def go(package, complete_deps \\ []) do
+    HTTPoison.get(@registry <> package <> "/latest")
+    |> handle_response()
+    |> get_deps()
+  end
+
+  defp get_deps(nil),  do: []
+  defp get_deps(deps),  do: Enum.map(deps, fn({k, _v}) -> k end)
+
+  defp handle_response({:ok, %Resp{status_code: 200, body: body}}), do:
+    Poison.decode!(body)["dependencies"]
+  defp handle_response(_), do: nil
 end
