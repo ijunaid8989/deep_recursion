c59d08ee4200c85ae566219c89aa762f75af7772
Junaid Farooq <junaid@evercam.io>
2019-07-14 18:41:45 +0500

    Get all unique deps on each levels.

--- lib/deep.ex
+++ lib/deep.ex
@@ -3,14 +3,16 @@ defmodule Deep do
 
   alias HTTPoison.Response, as: Resp
 
-  def go(package, complete_deps \\ []) do
+  def go(package) do
     HTTPoison.get(@registry <> package <> "/latest")
     |> handle_response()
     |> get_deps()
+    |> List.flatten()
+    |> Enum.uniq()
   end
 
   defp get_deps(nil),  do: []
-  defp get_deps(deps),  do: Enum.map(deps, fn({k, _v}) -> k end)
+  defp get_deps(deps), do: Enum.map(deps, fn({k, _v}) -> [k | go(k)] end)
 
   defp handle_response({:ok, %Resp{status_code: 200, body: body}}), do:
     Poison.decode!(body)["dependencies"]
